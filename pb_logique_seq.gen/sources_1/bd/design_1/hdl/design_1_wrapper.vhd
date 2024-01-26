340cfb417b25ce4482b0270e6ee7d7d40c199f2a
skid0u <felix.boivin@hotmail.com>
2024-01-25 23:15:40 -0500

    ouin bof
    bof

--- pb_logique_seq.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ pb_logique_seq.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
---Date        : Thu Jan 25 22:09:26 2024
+--Date        : Thu Jan 25 22:17:19 2024
 --Host        : DESKTOP-9AUGKDP running 64-bit major release  (build 9200)
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
--- pb_logique_seq.srcs/sources_1/imports/new/calcul_param_1.vhd
+++ pb_logique_seq.srcs/sources_1/imports/new/calcul_param_1.vhd
@@ -55,8 +55,94 @@ architecture Behavioral of calcul_param_1 is
 ---------------------------------------------------------------------------------------------
 --    Description comportementale
 ---------------------------------------------------------------------------------------------
+
+type fonction_etat is (
+     init,
+     att_enable,
+     compte1,
+     fois2,
+     save
+     );
+     
+    signal etat_present, etat_suivant: fonction_etat; 
+    signal compte : std_logic_vector (7 downto 0);
+    signal param_mem : unsigned (7 downto 0);   
+
 begin 
 
-     o_param <= x"01";    -- temporaire ...
+process(i_bclk, i_reset)
+    begin
+       if (i_reset = '1') then
+             etat_present <= init;
+       else
+           if rising_edge(i_bclk) then
+                 etat_present <= etat_suivant;
+           end if;
+       end if;
+end process;
+
+process(etat_present, i_en, i_ech, compte)
+begin
+    case etat_present is
+        when init =>       
+            etat_suivant <= att_enable;
+            compte <= "00000000";
+            param_mem <= "00000000";
+          
+        when att_enable =>
+            if i_en = '0' then
+                etat_suivant <= att_enable;
+            elsif i_en = '1' and i_ech(23) = '0' then
+                etat_suivant <= compte1;
+            elsif i_en = '1' and i_ech(23) = '1' then
+                etat_suivant <= fois2;
+            end if;
+                
+        when compte1 =>
+            if compte = "11111111" then
+                etat_suivant <= att_enable;
+            else
+                compte <= compte +1 ;
+                etat_suivant <= att_enable;
+            end if;
+           
+        when fois2 =>
+            if (compte >= 2) then
+                param_mem <= unsigned(compte) * 2;
+                etat_suivant <= save;
+            else
+                etat_suivant <= att_enable;
+            end if;
+         
+        when save =>
+            etat_suivant <= att_enable;
+               
+    end case;
+    
+end process;
+
+process(etat_present)
+begin
+    case etat_present is
+        when init =>       
+            
+          
+        when att_enable =>
+          
+            
+        when compte1 =>
+           
+
+        when fois2 =>
+            
+         
+        when save =>
+            
+               
+            
+    end case;
+    
+end process;
+
  
 end Behavioral;
